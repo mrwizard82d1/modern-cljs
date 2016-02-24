7961cfd7f8f013649707ffe16c2dc8ecd6b6104f
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-02-23 23:55:52 -0600

    Second edition tutorial 01

---
+++ modern-cljs/src/cljs/modern_cljs/core.cljs
@@ -0,0 +1,8 @@
+;; create the main project namespace
+(ns modern-cljs.core)
+
+;; enable ClojureScript to print to the JavaScript console of the browser
+(enable-console-print!)
+
+;; print to the console
+(println "Hello, Modern ClojureScript World!")
